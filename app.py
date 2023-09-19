f626c5e0ba41c7d9e71f1887445095867addd406
Михаил Шутов <mihvs22@gmail.com>
2023-09-19 14:21:34 +0300

    fix

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, request, Response
 
 app = Flask(__name__)
 
 
-@app.route('/mega/')
+@app.route('/mega', methods=['GET'])
 def mega():
-    print()
-    return ''
+    print(request.args.to_dict())
+    return Response(status="200 OK")
 
 
 if __name__ == '__main__':
